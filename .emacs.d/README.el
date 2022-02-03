365fcc14c336f9d6288b3abd72071245ab55012f
Malcolm <mango22mjk@gmail.com>
2022-02-03 15:36:01 -0500

    working on minor mode

--- .emacs.d/README.el
+++ .emacs.d/README.el
@@ -146,7 +146,7 @@
       :hook (after-init . doom-modeline-mode))
 
 (use-package org
-  :straight t
+  :ensure t
   :hook (org-mode . mk/org-mode-setup)
   :config
   (setq org-ellipsis " ▾"
@@ -195,4 +195,76 @@
 (use-package autothemer
   :straight t)
 
-(require lispy)
+(defun run-in-vterm-kill (process event)
+  "A process sentinel. Kills PROCESS's buffer if it is live."
+  (let ((b (process-buffer process)))
+    (and (buffer-live-p b)
+         (kill-buffer b))))
+
+(defun run-in-vterm (command)
+  "Execute string COMMAND in a new vterm.
+
+Interactively, prompt for COMMAND with the current buffer's file
+name supplied. When called from Dired, supply the name of the
+file at point.
+
+Like `async-shell-command`, but run in a vterm for full terminal features.
+
+The new vterm buffer is named in the form `*foo bar.baz*`, the
+command and its arguments in earmuffs.
+
+When the command terminates, the shell remains open, but when the
+shell exits, the buffer is killed."
+  (interactive
+   (list
+    (let* ((f (cond (buffer-file-name)
+                    ((eq major-mode 'dired-mode)
+                     (dired-get-filename nil t))))
+           (filename (concat " " (shell-quote-argument (and f (file-relative-name f))))))
+      (read-shell-command "Terminal command: "
+                          (cons filename 0)
+                          (cons 'shell-command-history 1)
+                          (list filename)))))
+  (with-current-buffer (vterm (concat "*" command "*"))
+    (set-process-sentinel vterm--process #'run-in-vterm-kill)
+    (vterm-send-string command)
+    (vterm-send-return)))
+
+(defun set-region-read-only (begin end)
+  "Sets the read-only text property on the marked region.
+
+Use `set-region-writeable' to remove this property."
+  ;; See http://stackoverflow.com/questions/7410125
+  (interactive "r")
+  (let ((modified (buffer-modified-p)))
+    (add-text-properties begin end '(read-only t))
+    (set-buffer-modified-p modified)))
+
+(defun set-region-writeable (begin end)
+  "Removes the read-only text property from the marked region.
+
+Use `set-region-read-only' to set this property."
+  ;; See http://stackoverflow.com/questions/7410125
+  (interactive "r")
+  (let ((modified (buffer-modified-p))
+        (inhibit-read-only t))
+    (remove-text-properties begin end '(read-only t))
+    (set-buffer-modified-p modified)))
+
+(defun mk/kill-line (arg)
+      "This is my first elips function it kills the current working line"
+      (interactive "P")
+
+    (beginning-of-line-text arg)
+  (kill-visual-line arg)
+
+
+
+      )
+(global-set-key "\C-q" 'mk/kill-line)
+
+(define-minor-mode mk/center-page-mode
+  "This is a minor mode that centers the page always"
+  :lighter center-mode
+
+  (recenter-top-bottom))
