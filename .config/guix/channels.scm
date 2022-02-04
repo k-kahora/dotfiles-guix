08d06ab3b95f9529091938eaacb8db56b418823b
Malcolm <mango22mjk@gmail.com>
2022-02-04 13:41:53 -0500

    Guix channels script and first flat pack pin

---
+++ .config/guix/channels.scm
@@ -0,0 +1,22 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "d7e1c5acb3c8282c6a568d97ab164db82e33324f")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (branch "master")
+        (commit
+          "ece373bcd1e00ef746fb7b104e56ab610cc4af2a")
+        (introduction
+          (make-channel-introduction
+            "33f86a4b48205c0dc19d7c036c85393f0766f806"
+            (openpgp-fingerprint
+              "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490")))))
