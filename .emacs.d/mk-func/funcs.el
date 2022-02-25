76657a88bc8fb1d2209476d8c4801eb50913ab19
Malcolm <mango22mjk@gmail.com>
2022-02-25 14:20:07 -0500

    new headerize func

---
+++ .emacs.d/mk-func/funcs.el
@@ -0,0 +1,16 @@
+(defun mk/format-cfiles ( exercise lab )
+    "Adds a header based on the car c code header to all c files - you can also customize which files it does this for"
+  (interactive "nEnter a lab: \nnEnter a Exercise: ")
+
+  
+  (let (list string)
+    (setq string (format "/*****************************************************************************/
+/*                 Malcolm Kahora CSC345-02 Lab %d Exercise %d                 */
+/*****************************************************************************/\n" lab exercise))
+    (setq list (find-file-noselect "*.el" nil nil t))
+    (dolist (element list)
+      (with-current-buffer element (beginning-of-buffer) (insert string))
+
+      )
+          
+    ))
